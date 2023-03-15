b506b11d9cefcdd6c544f0b9f02847f2668f442e
João Pedro Zamborlini Barcellos <20211TIIMI0087@cefetes.br>
2023-03-15 12:13:36 -0300

    Initial commit

---
+++ app/src/main/java/barcellos/zamborlini/app1/NextActivity.java
@@ -0,0 +1,20 @@
+package barcellos.zamborlini.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+        Intent intent = getIntent();
+        String text = intent.getStringExtra("Texto");
+        TextView tvExibir = findViewById(R.id.tvExibir);
+        tvExibir.setText(text);
+    }
+}
